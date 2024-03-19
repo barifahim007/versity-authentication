2cfd31dd3c309bf49e9b2e7e24c20e8f53cba54f
fahim007 <ehsanulbarifahim7@gmail.com>
2024-03-19 16:49:15 +0600

    check

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,10 @@
-import dotenv from "dotenv";
-import path from "path";
+import dotenv from 'dotenv'
+import path from 'path'
 
-dotenv.config({ path: path.join(process.cwd(), ".env") });
+dotenv.config({ path: path.join(process.cwd(), '.env') })
 
 export default {
   node_env: process.env.NODE_ENV,
   port: process.env.PORT,
-  database: process.env.DATABASE_URL,
-};
+  database: process.env.DATABASE_URL
+}
