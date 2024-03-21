ebcc6b09946b8967bab4f6ef9a150fc3a5be770b
fahim007 <ehsanulbarifahim7@gmail.com>
2024-03-21 17:12:09 +0600

    finished error handler

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,14 @@ import mongoose from 'mongoose'
 import app from './app'
 import config from './config/index'
 import { logger, errorLogger } from './shared/logger'
+import { Server } from 'http'
+
+// unchaughtExceptions
+process.on('uncaughtException', error => {
+  errorLogger.error(error), process.exit(1)
+})
+
+let server: Server
 
 async function main() {
   try {
@@ -15,6 +23,27 @@ async function main() {
   } catch (error) {
     errorLogger.error(`oh noo!!! database not connected ${error}`)
   }
+
+  // unhandled rejections
+
+  process.on('unhandledRejection', error => {
+    if (server) {
+      server.close(() => {
+        errorLogger.error(error), process.exit(1)
+      })
+    } else {
+      process.exit(1)
+    }
+  })
 }
 
 main()
+
+// sigterm
+
+process.on('SIGTERM', () => {
+  logger.info(`SIGTERM is  received.......`)
+  if (server) {
+    server.close()
+  }
+})
